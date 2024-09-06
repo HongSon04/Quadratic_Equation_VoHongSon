0361eb0736896c669cc94f162592335a7ee6ffb0
ChuBe04 <vohongson8520@gmail.com>
2024-09-06 14:03:04 +0700

    update code

--- solver.php
+++ solver.php
@@ -13,11 +13,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($delta < 0) {
         echo "Phương trình vô nghiệm.";
     } elseif ($delta == 0) {
-        $x = -$b / (2 * $a);
+        $x = round(-$b / (2 * $a), 2);
         echo "Phương trình có nghiệm kép: x = " . $x;
     } else {
-        $x1 = (-$b + sqrt($delta)) / (2 * $a);
-        $x2 = (-$b - sqrt($delta)) / (2 * $a);
+        $x1 = round((-$b + sqrt($delta)) / (2 * $a), 2);
+        $x2 = round((-$b - sqrt($delta)) / (2 * $a), 2);
         echo "Phương trình có 2 nghiệm phân biệt: x1 = " . $x1 . ", x2 = " . $x2;
     }
 }
